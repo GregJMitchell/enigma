3abf9312996e62e5aaa136a8592c30fda8b8dcee
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-19 16:42:24 -0600

    Create encrypt method for encrytion class

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,8 +1,29 @@
+require './lib/key_shift'
+
 class Encryption
+  include KeyShift
   attr_reader :message, :key, :date
   def initialize(message, key, date)
     @message = message
     @key = key
     @date = date
+  end
+
+  def encrypt
+    chars = ('a'..'z').to_a << ' '
+    lower_message = @message.downcase
+    keys = final_keys(@key, @date)
+    shifted = lower_message.chars.map do |letter|
+      value = (letter.ord - 97 + keys[0]) % 27
+      value = -1 if letter == ' '
+      character = chars[value]
+      keys.rotate!
+      character
+    end.join
+    encrypted = {}
+    encrypted[:encryption] = shifted
+    encrypted[:key] = key
+    encrypted[:date] = date
+    encrypted
   end
 end
