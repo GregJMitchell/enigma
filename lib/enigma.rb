3aee0a348597fcb563f81b61c26bf2751948f524
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-19 17:09:30 -0600

    Move decrypt logic out of enigma to decryption class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,15 +7,7 @@ class Enigma
   end
 
   def decrypt(message, key, date)
-    chars = ('a'..'z').to_a << ' '
-    lower_message = message.downcase
-    keys = final_keys(key, date)
-    lower_message.chars.map do |letter|
-      value = (letter.ord - 97 - keys[0]) % 27
-      value = -1 if letter == ' '
-      character = chars[value]
-      keys.rotate!
-      character
-    end.join
+    decryption = Decryption.new(message, key, date)
+    decryption.decrypt
   end
 end
