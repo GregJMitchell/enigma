9ee783c8ebc6d15baf9f119d8b3940dea1a99a23
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-19 16:22:37 -0600

    Create final keys method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,31 +3,12 @@ require_relative 'key_shift'
 class Enigma
   include KeyShift
   def encrypt(message, key, date)
-    chars = ("a".."z").to_a << " "
+    chars = ('a'..'z').to_a << ' '
     lower_message = message.downcase
-    # a_key = key[0..1].to_i
-    # b_key = key[1..2].to_i
-    # c_key = key[2..3].to_i
-    # d_key = key[3..4].to_i
-    # square_date = (date.to_i)**2
-    # date_key = square_date.to_s[-4..-1]
-    # a_key += date_key[0].to_i
-    # b_key += date_key[1].to_i
-    # c_key += date_key[2].to_i
-    # d_key += date_key[3].to_i
-    # keys = []
-    # keys << a_key
-    # keys << b_key
-    # keys << c_key
-    # keys << d_key
     keys = final_keys(key, date)
-
     shifted = lower_message.chars.map do |letter|
       value = (letter.ord - 97 + keys[0]) % 27
-      if letter == ' '
-        value = -1
-      end
-
+      value = -1 if letter == ' '
       character = chars[value]
       keys.rotate!
       character
@@ -40,29 +21,12 @@ class Enigma
   end
 
   def decrypt(message, key, date)
-    chars = ("a".."z").to_a << " "
+    chars = ('a'..'z').to_a << ' '
     lower_message = message.downcase
-    a_key = key[0..1].to_i
-    b_key = key[1..2].to_i
-    c_key = key[2..3].to_i
-    d_key = key[3..4].to_i
-    square_date = (date.to_i)**2
-    date_key = square_date.to_s[-4..-1]
-    a_key += date_key[0].to_i
-    b_key += date_key[1].to_i
-    c_key += date_key[2].to_i
-    d_key += date_key[3].to_i
-    keys = []
-    keys << a_key
-    keys << b_key
-    keys << c_key
-    keys << d_key
+    keys = final_keys(key, date)
     lower_message.chars.map do |letter|
       value = (letter.ord - 97 - keys[0]) % 27
-      if letter == ' '
-        value = -1
-      end
-
+      value = -1 if letter == ' '
       character = chars[value]
       keys.rotate!
       character
--- lib/key_shift.rb
+++ lib/key_shift.rb
@@ -1,8 +1,8 @@
 module KeyShift
   def date_shift(date)
-    square_date = (date.to_i)**2
+    square_date = date.to_i**2
     date_keys = square_date.to_s[-4..-1]
-    date_keys.split.to_i
+    date_keys.split('')
   end
 
   def key_shift(key)
@@ -15,6 +15,16 @@ module KeyShift
   end
 
   def final_keys(key, date)
-
+    date_keys = date_shift(date)
+    keys = key_shift(key)
+    # require 'pry'; binding.pry
+    real_keys = []
+    index = 0
+    4.times do
+      key_total = date_keys[index].to_i + keys[index]
+      real_keys << key_total
+      index += 1
+    end
+    real_keys
   end
 end
