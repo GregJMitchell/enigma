3064bcaaef671c2b31861801d5a20574ce457548
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-19 16:35:51 -0600

    Create test for readable attributes in encryption class

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -10,4 +10,15 @@ class EncryptionTest < Minitest::Test
 
     assert_instance_of Encryption, encryption
   end
+
+  def test_it_has_readable_attributes
+    message = ''
+    key = '0'
+    date = '0'
+    encryption = Encryption.new(message, key, date)
+
+    assert_equal '', Encryption.message
+    assert_equal '0', Encryption.key
+    assert_equal '0', Encryption.date
+  end
 end
