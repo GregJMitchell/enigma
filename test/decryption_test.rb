eaacf297ba3bab38f4c41a05c5318c97ac77b323
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-19 17:01:05 -0600

    Create test for readable attributes in decryption class

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -10,4 +10,15 @@ class DecryptionTest < Minitest::Test
 
     assert_instance_of Decryption, decryption
   end
+
+  def test_it_has_readable_attributes
+    message = ''
+    key = '0'
+    date = '0'
+    decryption = Decryption.new(message, key, date)
+
+    assert_equal '', decryption.message
+    assert_equal '0', decryption.key
+    assert_equal '0', decryption.date
+  end
 end
