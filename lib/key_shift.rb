9b331565655c07b930b6dcf5f5749f5f1f94b17d
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-19 16:10:57 -0600

    Create date shfit and key shift methods

--- lib/key_shift.rb
+++ lib/key_shift.rb
@@ -1,3 +1,20 @@
 module KeyShift
-  
+  def date_shift(date)
+    square_date = (date.to_i)**2
+    date_keys = square_date.to_s[-4..-1]
+    date_keys.split.to_i
+  end
+
+  def key_shift(key)
+    keys = []
+    keys << a_key = key[0..1].to_i
+    keys << b_key = key[1..2].to_i
+    keys << c_key = key[2..3].to_i
+    keys << d_key = key[3..4].to_i
+    keys
+  end
+
+  def final_keys(key, date)
+
+  end
 end
