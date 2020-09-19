8a282b635d1d2638b83dff95025393430e0c05f9
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-19 16:32:44 -0600

    Create test for initialization of encyrption class

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -2,5 +2,12 @@ require './test/test_helper'
 require './lib/encryption'
 
 class EncryptionTest < Minitest::Test
-  
+  def test_it_exists
+    message = ''
+    key = '0'
+    date = '0'
+    encryption = Encryption.new(message, key, date)
+
+    assert_instance_of Encryption, encryption
+  end
 end
