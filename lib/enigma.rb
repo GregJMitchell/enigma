257273b9622b96d125b284ba55c5154118ecf470
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-20 11:27:26 -0600

    Add call for generate date in encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,11 @@
 require_relative 'encryption'
 require_relative 'Decryption'
+require_relative 'generatable'
 
 class Enigma
+  include Generatable
   def encrypt(message, key = nil, date = nil)
+    date = generate_date if date.nil?
     encryption = Encryption.new(message, key, date)
     encryption.encrypt
   end
