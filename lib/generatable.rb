0a596bdca98ad93adbc0454bfdd09f9e2e5d1cd6
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-20 11:39:38 -0600

    Code generate date method

--- lib/generatable.rb
+++ lib/generatable.rb
@@ -1,5 +1,13 @@
+require 'date'
+
 module Generatable
   def generate_date
-    
+    date = DateTime.now
+    year = date.year.to_s[-2..-1]
+    day = date.day
+    month = date.month
+    day = "0#{day}" if day.to_s.length < 2
+    month = "0#{month}" if month.to_s.length < 2
+    "#{day}#{month}#{year}"
   end
 end
