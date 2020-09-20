fca2e6202c83f4ffcc9fc197f75c37944f0f0956
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-20 12:20:16 -0600

    Create the runner for decryption method

--- decrypt.rb
+++ decrypt.rb
@@ -1,1 +1,24 @@
-file_names = ARGV
+require './lib/enigma'
+
+file_names_key_date = ARGV
+
+enigma = Enigma.new
+file = File.open(file_names_key_date[0])
+file_data = file.read
+
+unless file_names_key_date[3].nil?
+  decrypted_message = enigma.decrypt(file_data, file_names_key_date[2], file_names_key_date[3])
+  file.close
+
+  File.open(file_names_key_date[1], "w") { |f| f.write "#{decrypted_message[:decryption]}" }
+  p "Created '#{file_names_key_date[1]}' with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
+end
+
+if file_names_key_date[3].nil?
+  decrypted_message = enigma.decrypt(file_data, file_names_key_date[2])
+  file.close
+
+  File.open(file_names_key_date[1], "w") { |f| f.write "#{decrypted_message[:decryption]}" }
+
+  p "Created '#{file_names_key_date[1]}' with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
+end
