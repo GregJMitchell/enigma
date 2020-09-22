60a37dd550702fc4bc1f0c7222f6a83478b680f8
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-22 05:12:42 -0600

    Refactor the generate key method to use wider range and add leading zero

--- lib/generatable.rb
+++ lib/generatable.rb
@@ -2,18 +2,14 @@ require 'date'
 
 module Generatable
   def generate_date
-    date = DateTime.now
-    year = date.year.to_s[-2..-1]
-    day = date.day
-    month = date.month
-    day = "0#{day}" if day.to_s.length < 2
-    month = "0#{month}" if month.to_s.length < 2
-    "#{day}#{month}#{year}"
+    Time.now.strftime("%d%m%y")
   end
 
   def generate_key
-    key = ['0']
-    key << rand(10_00..99_99).to_s
-    key.join
+    key = rand(10_00..99_999).to_s
+    until key.length == 5
+      key.insert(0, '0')
+    end
+    key
   end
 end
